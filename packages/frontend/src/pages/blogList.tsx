da7638ea87bdbf207edfa34ed3edcc8277091350
Eriec Tanijaya <eriectan88@gmail.com>
2025-09-24 11:53:02 +0700

    checkpoint

--- packages/frontend/src/pages/blogList.tsx
+++ packages/frontend/src/pages/blogList.tsx
@@ -1,3 +1,21 @@
+import type { ReactNode } from "react";
+
+type ButtonProps = {
+	children: ReactNode;
+};
+
+function Button({ children, ...props }: ButtonProps) {
+	return (
+		<button
+			type="button"
+			className="py-3 px-4 bg-slate-400 rounded-lg not-active:border-b-4 border-b-slate-800"
+			{...props}
+		>
+			{children}
+		</button>
+	);
+}
+
 export function BlogList() {
 	// todo: create a whole page first, and then separate the components
 
@@ -5,7 +23,14 @@ export function BlogList() {
 		<div className="flex flex-col">
 			<header>Header</header>
 			<main>Main</main>
-			<footer className="fixed bottom-0">Footer</footer>
+			<footer className="flex fixed bottom-0 w-full h-1/13 justify-evenly items-center bg-slate-200">
+				<Button>
+					<a href="#">Blogs</a>
+				</Button>
+				<Button>
+					<a href="#">Settings</a>
+				</Button>
+			</footer>
 		</div>
 	);
 }
