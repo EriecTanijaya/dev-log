77485ce5d58ef88d38a1b34f15b2c41e198abe43
Eriec Tanijaya <eriectan88@gmail.com>
2025-09-30 00:56:29 +0700

    refactor: remove unnecessary div

--- packages/frontend/src/components/header.tsx
+++ packages/frontend/src/components/header.tsx
@@ -1,12 +1,10 @@
 export function Header() {
 	return (
-		<header>
-			<div className="flex fixed w-full h-16 p-4 items-center bg-slate-200 top-0">
-				<p className="font-black">
-					{/** biome-ignore lint/suspicious/noCommentText: double slash for name */}
-					Eriec // Dev/Log
-				</p>
-			</div>
+		<header className="flex fixed w-full h-16 p-4 items-center bg-slate-200 top-0">
+			<p className="font-black">
+				{/** biome-ignore lint/suspicious/noCommentText: double slash for name */}
+				Eriec // Dev/Log
+			</p>
 		</header>
 	);
 }
