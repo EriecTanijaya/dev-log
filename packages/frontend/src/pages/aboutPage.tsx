a95cdf14b396b28fd8cd6b1e53f927ffac523b62
Eriec Tanijaya <eriec@grip-principle.com>
2025-10-02 18:03:15 +0700

    checkpoint

--- packages/frontend/src/pages/aboutPage.tsx
+++ packages/frontend/src/pages/aboutPage.tsx
@@ -6,7 +6,17 @@ export function AboutPage() {
 	return (
 		<div>
 			<Header />
-			<Main className="p-3">mantap</Main>
+			<Main className="p-3 flex flex-col gap-6">
+				<img
+					src="assets/blog-portrait-image.jpg"
+					alt="blog"
+					className="size-80 m-auto object-cover object-center"
+				/>
+
+				<h1 className="text-3xl font-bold">why i write this shitttttt</h1>
+
+				<p>mantap broooo</p>
+			</Main>
 			<Footer />
 		</div>
 	);
