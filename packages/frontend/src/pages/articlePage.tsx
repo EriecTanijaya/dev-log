198a49b5a1ccc572356f63d2258adea3a4129bd3
Eriec Tanijaya <eriectan88@gmail.com>
2025-10-02 13:21:16 +0700

    feat(article): scroll progress on footer

--- packages/frontend/src/pages/articlePage.tsx
+++ packages/frontend/src/pages/articlePage.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Footer } from "../components/footer";
+import { useState, useSyncExternalStore } from "react";
 import { Main } from "../components/main";
 
 // todo: add progress bar reading
@@ -30,6 +29,7 @@ export function ArticlePage() {
 					<p className="text-sm">29 JULY 1998 - BLOG</p>
 				</div>
 			</header>
+
 			<Main className="p-3 mb-16 flex flex-col gap-6">
 				<img
 					src="assets/blog-portrait-image.jpg"
@@ -119,7 +119,80 @@ export function ArticlePage() {
 					personal choice; it is a vital component of collective well-being.
 				</p>
 			</Main>
-			<Footer />
+
+			<footer className="fixed bottom-0 w-full bg-slate-200 h-16 flex flex-col justify-between">
+				<ScrollBar />
+
+				<div className="flex justify-evenly items-center">
+					<a
+						href="#"
+						className="border-b-4 border-b-slate-600 pb-[calc(1rem+env(safe-area-inset-bottom))]"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="32"
+							height="32"
+							fill="#000000"
+							viewBox="0 0 256 256"
+							className="fill-slate-600"
+						>
+							<title>explore icon</title>
+							<path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm51.58,57.79-32,64a4.08,4.08,0,0,1-1.79,1.79l-64,32a4,4,0,0,1-5.37-5.37l32-64a4.08,4.08,0,0,1,1.79-1.79l64-32A4,4,0,0,1,179.58,81.79Z"></path>
+						</svg>
+					</a>
+
+					<a href="#" className="pb-[calc(1rem+env(safe-area-inset-bottom))]">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="32"
+							height="32"
+							fill="#000000"
+							viewBox="0 0 256 256"
+							className="fill-slate-600"
+						>
+							<title>profile icon</title>
+							<path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path>
+						</svg>
+					</a>
+				</div>
+			</footer>
 		</div>
 	);
+}
+
+function ScrollBar() {
+	const scrollProgress = useScrollProgress();
+
+	return (
+		<div className="h-1.5 bg-slate-400">
+			<div
+				className="h-1.5 bg-teal-700"
+				style={{ width: `${scrollProgress}%` }}
+			></div>
+		</div>
+	);
+}
+
+function useScrollProgress(): number {
+	const scrollPosition = useSyncExternalStore(subscribe, getSnapshot);
+
+	function subscribe(callback: () => void) {
+		window.addEventListener("scroll", callback, { passive: true });
+
+		return () => {
+			window.removeEventListener("scroll", callback);
+		};
+	}
+
+	function getSnapshot() {
+		return document.body.scrollTop || document.documentElement.scrollTop;
+	}
+
+	const height =
+		document.documentElement.scrollHeight -
+		document.documentElement.clientHeight;
+
+	const scrolled = Math.ceil((scrollPosition / height) * 100);
+
+	return scrolled;
 }
